d851d829adbd9b503cfdc2e32841df31acf0bb49
Dimbeswar-Rabha <dimbeswar.rabha@talentica.com>
2020-03-27 00:36:38 +0530

    code generation process was starting even if code failed to create git branch

--- main_poc.py
+++ main_poc.py
@@ -31,39 +31,40 @@ if __name__ == '__main__':
 
     file_path = f'{DataCollectorPath}/stage/configuration/{file_name}'
     run_only_once = True
-    is_error_occured_in_creation_of_git_branch = False
-    try:
-        is_error_occured_in_creation_of_git_branch = create_git_branch(git_branch)
-        for test_case in test_cases:
-            with open(file_path, 'r') as f:
-                lines = f.readlines()
-            get_start_end_lines_of_tc_in_lines = get_start_and_end_line_number_of_tc(lines, test_case)
-            _, start_line_no_of_tc_in_lines, end_line_no_of_tc_in_lines = get_start_end_lines_of_tc_in_lines
-            # Extracting the required lines of test case from the entire file.
-            tc_lines = lines[start_line_no_of_tc_in_lines:end_line_no_of_tc_in_lines]
-            len_tc_lines = len(tc_lines)
-            while len_tc_lines > 0:
-                if tc_lines[len_tc_lines-1] == '\n':
-                    tc_lines.pop(len_tc_lines-1)
-                else:
-                    break
-                len_tc_lines -= 1
-            # Extracting the stage attributes from the parametrize statements.
-            unique_attributes = get_stage_attributes.get_stage_attributes(tc_lines, test_case,
-                                                                          "@pytest.mark.parametrize('stage_attributes'")
+    is_error_occured_in_creation_of_git_branch = create_git_branch(git_branch)
+    if not is_error_occured_in_creation_of_git_branch:
+        try:
+            for test_case in test_cases:
+                with open(file_path, 'r') as f:
+                    lines = f.readlines()
+                get_start_end_lines_of_tc_in_lines = get_start_and_end_line_number_of_tc(lines, test_case)
+                _, start_line_no_of_tc_in_lines, end_line_no_of_tc_in_lines = get_start_end_lines_of_tc_in_lines
+                # Extracting the required lines of test case from the entire file.
+                tc_lines = lines[start_line_no_of_tc_in_lines:end_line_no_of_tc_in_lines]
+                len_tc_lines = len(tc_lines)
+                while len_tc_lines > 0:
+                    if tc_lines[len_tc_lines-1] == '\n':
+                        tc_lines.pop(len_tc_lines-1)
+                    else:
+                        break
+                    len_tc_lines -= 1
+                # Extracting the stage attributes from the parametrize statements.
+                unique_attributes = get_stage_attributes.get_stage_attributes(tc_lines, test_case,
+                                                                              "@pytest.mark.parametrize('stage_attributes'")
 
-            output_from_read_input_data_from_json_file = read_input_data.read_input_data_from_json_file(
-                'data/input_data.json', test_case, unique_attributes)
-            integrate_functions.integrate_test_case(run_only_once, file_name, lines, tc_lines,
-                                                    test_case, get_start_end_lines_of_tc_in_lines,
-                                                    output_from_read_input_data_from_json_file)
-            run_only_once = False
-    except Exception as error:
-        # if any exception occurs above (such as file not found) then this flag will be set to True
-        logger.error(error)
-        if not is_error_occured_in_creation_of_git_branch:
+                output_from_read_input_data_from_json_file = read_input_data.read_input_data_from_json_file(
+                    'data/input_data.json', test_case, unique_attributes)
+                integrate_functions.integrate_test_case(run_only_once, file_name, lines, tc_lines,
+                                                        test_case, get_start_end_lines_of_tc_in_lines,
+                                                        output_from_read_input_data_from_json_file)
+                run_only_once = False
+        except Exception as error:
+            # if any exception occurs above (such as file not found) then this flag will be set to True
+            logger.error(error)
             os.chdir(f'{DataCollectorPath}')
             os.system('git checkout master')
             os.system(f'git branch -D {git_branch}')
-            logger.exception(error)
             logger.info(f'git branch {git_branch} has been deleted')
+
+    # if code fails to create git branch
+    logger.info(f'code generation process did not start.Git branch{git_branch} was not created')
